10a51a69e40dacb85c88e3754c32385c89401052
houyishetian <lisonglin234@163.com>
2017-10-14 01:44:35 +0800

    fix when softinput displays, the sliding menu will scroll to end

--- app/src/main/java/com/lin/read/fragment/ScanFragment.java
+++ app/src/main/java/com/lin/read/fragment/ScanFragment.java
@@ -1,23 +1,23 @@
 package com.lin.read.fragment;
 
 
-import android.animation.StateListAnimator;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.lin.read.R;
@@ -52,10 +52,21 @@ public class ScanFragment extends Fragment {
     private EditText wordsNumEt;
     private EditText recommendEt;
 
+    private ScrollView scrollView;
+
+    //添加用来在键盘显示的时候，改变该view的高度（键盘高度），从而把layout顶上去；若是键盘隐藏，则将该view高度设置为0
+    private View tempViewForSoft;
+
+    //用来记录上次监听到的屏幕高度变化时的高度，避免重复处理，否则会陷入死循环
+    int lastHeight = -1;
+
+    private Handler handler;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_scan, null);
         initView(view);
+        setSoftInputStateListener();
         return view;
     }
 
@@ -70,10 +81,16 @@ public class ScanFragment extends Fragment {
         scanRankTypeRcv = (RecyclerView) view.findViewById(R.id.rcv_scan_rank);
         scanBookTypeRcv = (RecyclerView) view.findViewById(R.id.rcv_scan_booktype);
 
-        scoreEt= (EditText) view.findViewById(R.id.et_socre);
-        scoreNumEt= (EditText) view.findViewById(R.id.et_socre_num);
-        wordsNumEt= (EditText) view.findViewById(R.id.et_words_num);
-        recommendEt= (EditText) view.findViewById(R.id.et_recommend);
+        scoreEt = (EditText) view.findViewById(R.id.et_socre);
+        scoreNumEt = (EditText) view.findViewById(R.id.et_socre_num);
+        wordsNumEt = (EditText) view.findViewById(R.id.et_words_num);
+        recommendEt = (EditText) view.findViewById(R.id.et_recommend);
+
+        scrollView = (ScrollView) view.findViewById(R.id.scroll_view);
+
+        tempViewForSoft = view.findViewById(R.id.tempView_for_soft);
+
+        handler=new Handler();
 
         setAdapter();
 
@@ -103,17 +120,17 @@ public class ScanFragment extends Fragment {
 
         scanWebTypeRcv.setLayoutManager(new GridLayoutManager(getActivity(), 4));
         scanWebTypeAdapter.setDefaultChecked("起点");
-        scanWebTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(),15));
+        scanWebTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(), 15));
         scanWebTypeRcv.setAdapter(scanWebTypeAdapter);
 
         scanRankTypeRcv.setLayoutManager(new GridLayoutManager(getActivity(), 4));
         scanRankTypeAdapter.setDefaultChecked("推荐");
-        scanRankTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(),15));
+        scanRankTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(), 15));
         scanRankTypeRcv.setAdapter(scanRankTypeAdapter);
 
         scanBookTypeRcv.setLayoutManager(new GridLayoutManager(getActivity(), 4));
         scanBookTypeAdapter.setDefaultChecked("玄幻");
-        scanBookTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(),15));
+        scanBookTypeRcv.addItemDecoration(new ScanTypeItemDecoration(getActivity(), 15));
         scanBookTypeRcv.setAdapter(scanBookTypeAdapter);
     }
 
@@ -146,10 +163,86 @@ public class ScanFragment extends Fragment {
         scanFilterLayout.setVisibility(View.GONE);
     }
 
-    private void setInputFilter(){
+    private void setInputFilter() {
         scoreEt.setFilters(new InputFilter[]{new ScoreInputFilter()});
         scoreNumEt.setFilters(new InputFilter[]{new NumberInputFilter(6)});
         wordsNumEt.setFilters(new InputFilter[]{new NumberInputFilter(4)});
         recommendEt.setFilters(new InputFilter[]{new NumberInputFilter(4)});
+    }
+
+    /**
+     * 设置键盘状态的监听
+     */
+    private void setSoftInputStateListener() {
+        scrollView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            //当键盘弹出隐藏的时候会 调用此方法。
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                //获取当前界面可视部分
+                getActivity().getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+                //获取屏幕的高度
+                int screenHeight = getActivity().getWindow().getDecorView().getRootView().getHeight();
+                //此处就是用来获取键盘的高度的， 在键盘没有弹出的时候 此高度为0 键盘弹出的时候为一个正数
+                int heightDifference = screenHeight - r.bottom;
+                if (heightDifference == 0) {
+                    android.util.Log.e("Test", "hide softInput!---"+heightDifference);
+                } else {
+                    android.util.Log.e("Test", "show softInput!---"+heightDifference);
+                }
+
+                //若当前height改变还未处理过
+                if (lastHeight != heightDifference) {
+                    //将该height设置到tempViewForSoft
+                    ViewGroup.LayoutParams params = tempViewForSoft.getLayoutParams();
+                    params.height = heightDifference;
+                    tempViewForSoft.setLayoutParams(params);
+                    //若此时是键盘显示
+                    if (heightDifference != 0) {
+                        scrollToEndAndRequestFocus();
+                    }
+                    //将当前高度记为已处理，否则fullScroll会requestLayout,会再次触发onGlobalLayout，这样会陷入死循环
+                    lastHeight = heightDifference;
+                }
+            }
+        });
+    }
+
+    private void scrollToEndAndRequestFocus(){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //获取触发键盘的EditText
+                EditText currentFocusEt = getFocusEt();
+                //scrollview滚动到末尾
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                //之前触发键盘的EditText重新获取焦点
+                if (currentFocusEt != null) {
+                    currentFocusEt.setFocusable(true);
+                    currentFocusEt.requestFocus();
+                }
+            }
+        });
+    }
+
+    /**
+     * 获取触发键盘时获取焦点的view
+     * @return
+     */
+    private EditText getFocusEt() {
+
+        if (scoreEt.hasFocus()) {
+            return scoreEt;
+        }
+        if (scoreNumEt.hasFocus()) {
+            return scoreNumEt;
+        }
+        if (wordsNumEt.hasFocus()) {
+            return wordsNumEt;
+        }
+        if (recommendEt.hasFocus()) {
+            return recommendEt;
+        }
+        return null;
     }
 }
