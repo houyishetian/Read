2df085c3d7eec66e0f34f96e3477ef6f2b07545d
houyishetian <lisonglin234@163.com>
2017-10-16 00:22:03 +0800

    change wording about scaning

--- app/src/main/java/com/lin/read/utils/Constants.java
+++ app/src/main/java/com/lin/read/utils/Constants.java
@@ -6,10 +6,10 @@ package com.lin.read.utils;
 
 public class Constants {
     public static final String KEY_SEARCH_INFO="KEY_SEARCH_INFO";
-    public static final String TEXT_SCAN_START="正在扫描书籍信息...";
-    public static final String TEXT_SCAN_BOOK_INFO_END="扫描书籍信息结束，共%d本书！";
-    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_START="正在过滤书籍信息...";
-    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_GET_ONE="正在过滤书籍信息，有%d本符合条件...";
-    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_END="过滤书籍信息结束，共%d本符合条件！";
+    public static final String TEXT_SCAN_START="正在检索书籍...";
+    public static final String TEXT_SCAN_BOOK_INFO_END="检索书籍结束，共%d本书！";
+    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_START="正在扫描书籍...";
+    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_GET_ONE="扫描书籍，扫描到%d本...";
+    public static final String TEXT_SCAN_BOOK_INFO_BY_CONDITION_END="扫描书籍结束，共扫描到%d本！";
 
 }
