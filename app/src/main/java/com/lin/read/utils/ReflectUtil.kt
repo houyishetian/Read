222ca0c069429d772d71befd2be7632e3e9fb9e5
houyishetian <lisonglin234@163.com>
2019-09-12 20:56:08 +0800

    replace the java reflect types mapping with library implementation

--- app/src/main/java/com/lin/read/utils/ReflectUtil.kt
+++ app/src/main/java/com/lin/read/utils/ReflectUtil.kt
@@ -1,10 +1,6 @@
 package com.lin.read.utils
 
 import android.util.Log
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
 
 class ReflectUtil {
     companion object {
@@ -15,23 +11,12 @@ class ReflectUtil {
         }
 
         fun <T> invokeMethod(obj: Any, methodName: String, returnClz: Class<T>, vararg params: Any): T? {
-            fun parseClass(ori: Class<*>): Class<*> {
-                return when (ori) {
-                    java.lang.Integer::class.java -> Int::class.java
-                    java.lang.Byte::class.java -> Byte::class.java
-                    java.lang.Short::class.java -> Short::class.java
-                    java.lang.Long::class.java -> Long::class.java
-                    java.lang.Double::class.java -> Double::class.java
-                    java.lang.Float::class.java -> Float::class.java
-                    java.lang.Character::class.java -> Char::class.java
-                    java.lang.Boolean::class.java -> Boolean::class.java
-                    else -> ori
-                }
-            }
             obj.javaClass.methods.forEach {
                 Log.d("available item", "${it.name},params type:${it.parameterTypes.map { it.name }.joinToString("/")}")
             }
-            return obj.javaClass.getMethod(methodName, *params.map { parseClass(it.javaClass) }.toTypedArray()).invoke(obj, *params).let {
+            return obj.javaClass.getMethod(methodName, *params.map {
+                it::class.javaPrimitiveType ?: it.javaClass
+            }.toTypedArray()).invoke(obj, *params).let {
                 (it as? T) ?: returnClz.cast(it)
             }
         }
